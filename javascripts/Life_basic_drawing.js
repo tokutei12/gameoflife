69cd5f45ef76e134160e6ae2b2dd248061a23721
Kimberly Toy <tokutei12@gmail.com>
2013-09-12 23:24:16 -0400

    make use of Coord class, add some comments

--- javascripts/Life_basic_drawing.js
+++ javascripts/Life_basic_drawing.js
@@ -19,16 +19,20 @@
 
 	var paint = function(){
 		for (var i = 0; i < grid_x; i++) {
+			//draw vertical grid lines
 			pad.draw_line(Coord(pad.get_width()*i/grid_x, 0), Coord(pad.get_width()*i/grid_x, 
 				pad.get_height()), grid_thickness, grey);
 			for (var j = 0; j < grid_y; j++) {
+				//draw horizontal grid lines
 				pad.draw_line(Coord(0, pad.get_height()*j/grid_y), Coord(pad.get_width(), 
 					pad.get_height()*j/grid_y), grid_thickness, grey);
-				// draw white squares for cells that are alive
+				// draw white squares for cells that are alive, black for nonliving
 				if (life.container[i][j] === true) {
-					pad.draw_rectangle({x:pad.get_width()*i/grid_x+grid_thickness, y:pad.get_height()*j/grid_y+grid_thickness}, pad.get_width()/grid_x-2*grid_thickness, pad.get_height()/grid_y-2*grid_thickness, 0, white, white );
+					pad.draw_rectangle(Coord(pad.get_width()*i/grid_x+grid_thickness, pad.get_height()*j/grid_y+grid_thickness), 
+						pad.get_width()/grid_x-2*grid_thickness, pad.get_height()/grid_y-2*grid_thickness, 0, white, white );
 				} else {
-					pad.draw_rectangle({x:pad.get_width()*i/grid_x+grid_thickness, y:pad.get_height()*j/grid_y+grid_thickness}, pad.get_width()/grid_x-2*grid_thickness, pad.get_height()/grid_y-2*grid_thickness, 0, black, black );
+					pad.draw_rectangle(Coord(pad.get_width()*i/grid_x+grid_thickness, pad.get_height()*j/grid_y+grid_thickness), 
+						pad.get_width()/grid_x-2*grid_thickness, pad.get_height()/grid_y-2*grid_thickness, 0, black, black );
 				}
 			}
 		}
