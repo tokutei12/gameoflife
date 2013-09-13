135c4206458d0df089a2041e505567e403d609f8
Kimberly Toy <tokutei12@gmail.com>
2013-09-12 21:53:39 -0400

    Refactor paint methods in Life_basic_drawing, reorganize file structure, add some basic styling

---
+++ javascripts/Life_basic_drawing.js
@@ -0,0 +1,61 @@
+(function () {
+	// define some colors
+	var life = new LifeContainer(10);
+	var black = Color(0,0,0);
+	var red = Color(255,0,0);
+	var green = Color(0,255,0);
+	var grey = Color(34,34,34);
+	var white = Color(255,255,255);
+    
+	// create the drawing pad object and associate with the canvas
+	pad = Pad(document.getElementById('canvas'));
+	pad.clear();
+    
+	// set constants to be able to scale to any canvas size
+	var MAX_X = 110;
+	var MAX_Y = 110;
+	var x_factor = pad.get_width() / MAX_X;
+	var y_factor = pad.get_height() / MAX_Y;
+
+	// draw some circles and squares inside
+	var RADIUS = 5;
+	var LINE_WIDTH = 2;
+
+	var paint = function(){
+		for (var i = 10; i < MAX_X; i = i + 10) {
+			pad.draw_line({x:pad.get_width()*i/100, y:0}, {x:pad.get_width()*i/100, y:400}, 5, grey);
+			for (var j = 10; j < MAX_Y; j = j + 10) {
+				pad.draw_line({x:0, y:pad.get_height()*j/100}, {x:400, y:pad.get_height()*j/100}, 5, grey);
+				// select circle or square according some arbitrary criterion
+				if (life.container[i / 10 - 1][j / 10 - 1].alive) {
+					pad.draw_rectangle({x:pad.get_width()*(i/10-1)/10+5, y:pad.get_height()*(j/10-1)/10+5}, pad.get_width()/10-10, pad.get_height()/10-10, 0, white, white );
+				} else {
+					pad.draw_rectangle({x:pad.get_width()*(i/10-1)/10+5, y:pad.get_height()*(j/10-1)/10+5}, pad.get_width()/10-10, pad.get_height()/10-10, 0,black, black );
+					}
+				}
+		}
+	};
+
+	paint();
+
+	var repaint = function(){
+		pad.clear();
+		life.step();
+		paint();
+	};
+
+	var reset = function(){
+		console.log("reset");
+		life = new LifeContainer(10);
+		pad.clear();
+		paint();
+	};
+
+	// add event listener to step button 
+	var step_button = document.getElementById("nextstep");
+	step_button.addEventListener("click", repaint, false);
+
+	//add event listener to reset button
+	var reset_button = document.getElementById("reset");
+	reset_button.addEventListener("click", reset, false);
+	}) ()
